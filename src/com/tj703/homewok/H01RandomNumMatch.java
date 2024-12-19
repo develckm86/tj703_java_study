0401088ea102b6b129a70869808f2958b1ddce87
develckm86 <develckm86@gmail.com>
2024-12-19 10:16:09 +0900

    반복문의 break continue

--- src/com/tj703/homewok/H01RandomNumMatch.java
+++ src/com/tj703/homewok/H01RandomNumMatch.java
@@ -5,24 +5,22 @@ import java.util.Scanner;
 public class H01RandomNumMatch {
     public static void main(String[] args) {
         System.out.println("랜덤한게 생성한 (1~50)수를 맞추세요! ");
-        //맞출때 까지 반복 실행
-        double random=Math.random(); //java.lang 패키지는 자동으로 Import (String,Integer..)
-        System.out.println(random);
-        long num=(long) (random*50)+1; //더큰수->작은수 (강제형변환) casting
-        //(int)13.12345611223 : 실수 타입이 정수가 될때는 소수점 버림이 발생
-        //let num=  Math.trunc(-1.4); //js에서 버림 1
-        //let num=  Number.parseInt(-1.4); //js에서 버림 2
-
-        //fool(내림) : 1.4 -> 1, -1.4 => -2
-        //ceil(올림),round(반올림)
-
-
+        double random=Math.random();
+        long num=(long) (random*50)+1;
         System.out.println(num);
-        Scanner scanner=new Scanner(System.in);
-        String inputNumStr=scanner.nextLine();
-        //enter(line 개행)를 입력할때까지의 문자열을 받는다.
-        //콘솔에서 문자열을 입력하고 엔터를 누를때까지 jvm 대기
-        System.out.println("당신이 입력한 수는 :"+inputNumStr);
-        //랜덤하게 뽑은 수가 입력한 수와 같을때까지 반복실행하는 코드를 작성하세요!
+        while(true){
+            Scanner scanner=new Scanner(System.in);
+            String inputNumStr=scanner.nextLine();
+            try{
+                int inputNum=Integer.parseInt(inputNumStr);
+                //Integer.parseInt : "13"=>13 (Number() or Number.parsInt())
+                //"아13" => 오류
+            }catch (NumberFormatException e){
+                System.out.println("수만입력하세요!");
+                continue;//아래코드는 무시하고 다시 반복문 실행
+            }
+            System.out.println("당신이 입력한 수는 :"+inputNumStr);
+            //break; //반복문을 종료
+        }
     }
 }
