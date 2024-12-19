a68ca5a44f3a159b6130cb293e88c23210395b26
develckm86 <develckm86@gmail.com>
2024-12-19 12:00:02 +0900

    게임 무한 반복 완성 및 외부 반복문 종료

--- src/com/tj703/homewok/H01RandomNumMatch.java
+++ src/com/tj703/homewok/H01RandomNumMatch.java
@@ -4,41 +4,44 @@ import java.util.Scanner;
 
 public class H01RandomNumMatch {
     public static void main(String[] args) {
-        System.out.println("랜덤한게 생성한 (1~50)수를 맞추세요! ");
-        double random=Math.random();
-        int num=(int) (random*50)+1;
-        System.out.println(num);
         boolean game=true;
-        int count=0;
-        while(game){
-            Scanner scanner=new Scanner(System.in);
-            String inputNumStr=scanner.nextLine();
-            //정답을 5번 안에 맞추도록 하세요!
-            //입력이 5번을 넘기면 졌습니다. 종료! **1
-            //game을 무한히 반복시키세요! 2
-            try {
-                //1+(--count) : 1을 먼저더하고 count를 1빼라
-                //1+(count--) : count를 1빼고 1을 더하라
-                int inputNum=Integer.parseInt(inputNumStr);
-                ++count;
-                //다음 코드부터는 inputNum은 무조건 존재함
-                if(inputNum==num){
-                    System.out.println(count+" 시도 :정답!"); //break;
-                    break;
+        while(game) {
+            System.out.println("랜덤한게 생성한 (1~50)수를 맞추세요! ");
+            double random = Math.random();
+            int num = (int) (random * 50) + 1;
+            System.out.println(num);
+            int count = 0;
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String inputNumStr = scanner.nextLine();
+                //game을 무한히 반복시키세요! 2
+                try {
+                    //1+(--count) : 1을 먼저더하고 count를 1빼라
+                    //1+(count--) : count를 1빼고 1을 더하라
+                    int inputNum = Integer.parseInt(inputNumStr);
+                    ++count;
+                    //다음 코드부터는 inputNum은 무조건 존재함
+                    if (inputNum == num) {
+                        System.out.println(count + " 시도 :정답!"); //break;
+                        break;
+                    }
+                    if (count == 5) {
+                        System.out.println("졌습니다.");
+                        break;
+                    }
+                    if (num < inputNum) {
+                        System.out.println("down");
+                    } else {
+                        System.out.println("up");
+                    }
+                } catch (NumberFormatException e) {
+                    if(inputNumStr.equals("exit")){
+                        System.out.println("어플이 종료됩니다.");
+                        game=false; //전체 while 를 break 할 수 없어서 game=false 로 종료
+                        break; //내부 while break;
+                    }
+                    System.out.println("숫자만 입력하세요.");
                 }
-                if(count==5){
-                    System.out.println("졌습니다.");
-                    break;
-                }
-                if(num<inputNum){
-                    System.out.println("down");
-                }else{
-                    System.out.println("up");
-                }
-
-
-            }catch (NumberFormatException e){
-                System.out.println("숫자만 입력하세요.");
             }
         }
     }
