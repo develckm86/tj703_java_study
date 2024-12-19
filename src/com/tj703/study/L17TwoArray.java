e1be9593feb14f40ca77fe4b9f08ca1ecbb6e344
develckm86 <develckm86@gmail.com>
2024-12-19 13:17:48 +0900

    for i 로 strArr 모든 데이터 더하기

--- src/com/tj703/study/L17TwoArray.java
+++ src/com/tj703/study/L17TwoArray.java
@@ -5,29 +5,29 @@ public class L17TwoArray {
         //2차원 배열에 대해 알아보자
         int [] arr={12,13,14};
         int [][] twoArr={{1,2,3},{4,5,6},{7,8,9}};
-        String [] strArr={"123","456","789"};
+        String [] strArr={"1234","567","891011"};
         String str ="안녕하세요~"; //{'안','녕','하','세','요'}
         System.out.println(str.charAt(2)); //str[2]
-        //for(char c : str){}
-        char [] cArr=str.toCharArray();
-        for(char c: cArr){
-            System.out.print(c+",");
-        }
         System.out.println("\n{\"123\",\"456\",\"789\"} 반복실행");
-        for(String s: strArr){
-           char [] cs=s.toCharArray();
-           for(char c : cs ){
-               System.out.print(c+",");
-           }
+        int sum=0;
+
+        for(int i=0; i<strArr.length; i++){
+            String s=strArr[i];
+            for(int j=0; j<s.length(); j++){
+                System.out.print(s.charAt(j)+",");
+                //sum+=Integer.parseInt(String.valueOf(s.charAt(j)).toString());
+                sum+=Integer.parseInt(s.charAt(j)+"");
+            }
             System.out.println();
         }
+        System.out.println("strArr의 모든 데이터의 합 :"+sum);
+        sum=0;
         //twoArr 의 모든 데이터를 더해서 출력하세요!
-        int sum=0;
         for (int[] nums:twoArr){
             for(int num : nums){
                 sum+=num;
             }
         }
-        System.out.println(sum);
+        System.out.println("towArr의 모든 데이터의 합 :"+sum);
     }
 }
