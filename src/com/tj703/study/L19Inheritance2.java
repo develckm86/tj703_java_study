09ebad665567cb7df45ea151022b6a6b6cf41d74
develckm86 <develckm86@gmail.com>
2024-12-20 13:34:00 +0900

    상속과 생성자

--- src/com/tj703/study/L19Inheritance2.java
+++ src/com/tj703/study/L19Inheritance2.java
@@ -15,12 +15,18 @@ public class L19Inheritance2 {
     class C extends B{
         //자동으로 만들어지는 default 생성자 == public C(){super();}
         //B의 생성자 규칙이 바뀌면 default 생성자가 부모 객체를 만들 수 없기 때문에 생성자를 재정의 해야한다.
+        public C(){
+            //super(); 자동으로 작성된다. (매개변수가 없는 생성자가 있을때 )
+            super(100);
+
+        }
+
     }
     class B extends A{ //부모가 생성자를 정의하고 있을때 오류 => B가 부모의 생성자를 호출하면 오류가 해결됨
         //B에서 A와 거의 유사한데 조금 다르게 구현하고 싶다.... -> 상속
         //B의 부모로 A를 지정하면 A가 정의하는 모든 필드를 B가 사용할 수 있다. B extends A
         int a=33;
-        B(){
+        B(int a){
             //int c=30; //부모가 생성자를 강제하면 자식에서 부모의 생성자를 호출하기 전까지는 아무것도 할 수 없다.
             //super(this.a);//super() 부모의 생성자를 호출할때는 자식필드(this)는 호출할 수 없다.
             super(33);
@@ -33,7 +39,7 @@ public class L19Inheritance2 {
 
     public static void main(String[] args) {
         L19Inheritance2 o = new L19Inheritance2();
-        B b = o.new B();
+        B b = o.new B(1);
         System.out.println(b.a);
         System.out.println(b.b);
         System.out.println(b.add(13,44));
---
+++ src/com/tj703/study/L20InheritanceConstruct.java
@@ -0,0 +1,35 @@
+package com.tj703.study;
+
+public class L20InheritanceConstruct {
+    class A{
+        int a;
+        A(int a){
+            this.a=a;
+        }
+    }
+    //이때 생기는 문제를 해결하세요! 오늘의 문제~ 내일!!
+    class B extends A{
+        int b;
+        B(int a,int b){
+            super(a);
+            this.b=b;
+        }
+    }
+    class C extends B{
+        //C가 B를 상속받았기 때문에 생성자 규칙을 따라야한다.
+        //=>B를 객체로 만들기 위해 C의 생성자에서 B의 생성자를 꼭 호출해야한다.
+        int c;
+        C(int a, int b, int c){
+            super(a,b);
+            this.c=c;
+        }
+    }
+    public static void main(String[] args) {
+        L20InheritanceConstruct o=new L20InheritanceConstruct();
+        C c=o.new C(10,20,30);
+        System.out.println(c.c); //C.c
+        System.out.println(c.b); //B.b
+        System.out.println(c.a); //A.a
+
+    }
+}
