98916b567608b7f759c00fddd8b524e827acbbd6
develckm86 <develckm86@gmail.com>
2024-12-23 12:40:40 +0900

    접근지정자 public default protected private

---
+++ src/com/tj703/homewok/AccessTest.java
@@ -0,0 +1,8 @@
+package com.tj703.homewok;
+
+public class AccessTest {
+    public String str= "퍼블릭한 str";
+    String str2= "기본 str";
+    protected String str3= "보호받는 str";
+    private String str4="개인적인 str";
+}
