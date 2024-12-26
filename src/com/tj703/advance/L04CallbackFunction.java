72ea8ab06f0714cbc65e85533db6f31b813a35de
develckm86 <develckm86@gmail.com>
2024-12-26 11:23:11 +0900

    콜백함수를 타입으로 명시해서 정의

--- src/com/tj703/advance/L04CallbackFunction.java
+++ src/com/tj703/advance/L04CallbackFunction.java
@@ -6,28 +6,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class L04CallbackFunction {
+    JFrame frame=null;
+    JButton btn=null;
+    JLabel label=null;
+    class BtnHandler implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setText(label.getText()+"클릭");
+        }
+    }
+    //main 완전히 다른영역 => 클래스의 필드를 사용하려면 인스턴스화(객체생성)
     public static void main(String[] args) {
-        JFrame frame=new JFrame("콜백함수의 이해");
-        //frame.setSize(300,300);
-        frame.setBounds(500,1500,300,300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton btn=new JButton("버튼입니다.");
-        //<html><p id="container"><button></></></>
-        //container.append(btn);
-        frame.add(btn, BorderLayout.SOUTH); //add(Component c)
-        //상수(바뀌지 않는 수)의 개념 : 저장된 정보를 호출하기 위해 사용됨
-        JLabel label=new JLabel("나는 라벨입니다.");
-        frame.add(label);
-        btn.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String txt=label.getText();
-                txt+="\n클릭";
-                label.setText(txt);
-            }
-        }); //Action==click
-        //인스턴스 == 객체
-        frame.setVisible(true);
-
+        L04CallbackFunction o=new L04CallbackFunction();
+        o.frame=new JFrame("콜백함수의 이해");
+        o.frame.setBounds(500,1500,300,300);
+        o.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        o.btn=new JButton("버튼입니다.");
+        o.btn.addActionListener(o.new BtnHandler());
+        o.frame.add(o.btn, BorderLayout.SOUTH);
+        o.label=new JLabel("나는 라벨입니다.");
+        o.frame.add(o.label);
+        o.frame.setVisible(true);
     }
 }
